0b6d298c75883feb47c827ce4aa5a1a88e575d68
aa-ag <aaron.aguerrevere@gmail.com>
2020-12-30 10:20:12 -0600

    first commit

---
+++ script.py
@@ -0,0 +1,32 @@
+###--- IMPORTS ---###
+import re
+
+###--- FUNCTIONS ---###
+
+
+def check_url_validity_(userinput):
+    # regular expression to check if URL is valid
+    regex = ("(www.)?" +
+             "[a-zA-Z0-9@:%._\\+~#?&//=]" +
+             "{2,256}\\.[a-z]" +
+             "{2,6}\\b([a-zA-Z0-9@:%._\\+~#?&//=]*)")
+
+    compiled = re.compile(regex)
+
+    if userinput == None or userinput == '':
+        return False
+
+    else:
+        if re.search(compiled, userinput):
+            return True
+        else:
+            return False
+
+
+###--- DRIVER CODE ---###
+if __name__ == "__main__":
+    userinput = input("Please enter a URL and hit Enter: ")
+    if check_url_validity_(userinput) == True:
+        print("Valid.")
+    else:
+        print("Not valid.")
