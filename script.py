98bf66dc519c742b5f441ccc3fded1b8cf60eb13
aa-ag <aaron.aguerrevere@gmail.com>
2020-12-30 10:36:10 -0600

    changed to requests as it checks existence instead of validity of format

--- script.py
+++ script.py
@@ -1,32 +1,45 @@
 ###--- IMPORTS ---###
 import re
+import requests
+
 
 ###--- FUNCTIONS ---###
 
+# [!] This approached is flawed as many thins can be considered valid URLs
+# It also requires more work to try to catch all posissibilities via regex
 
-def check_url_validity_(userinput):
-    # regular expression to check if URL is valid
-    regex = ("((http|https)://)(www.)?" +
-             "[a-zA-Z0-9@:%._\\+~#?&//=]" +
-             "{2,256}\\.[a-z]" +
-             "{2,6}\\b([a-zA-Z0-9@:%._\\+~#?&//=]*)")
+# def check_url_validity(userinput):
+#     # regular expression to check if URL is valid
+#     regex = ("((http|https)://)(www.)?" +
+#              "[a-zA-Z0-9@:%._\\+~#?&//=]" +
+#              "{2,256}\\.[a-z]" +
+#              "{2,6}\\b([a-zA-Z0-9@:%._\\+~#?&//=]*)")
 
-    compiled = re.compile(regex)
+#     compiled = re.compile(regex)
 
-    if userinput == None or userinput == '':
-        return False
+#     if userinput == None or userinput == '':
+#         return False
 
+#     else:
+#         if re.search(compiled, userinput):
+#             return True
+#         else:
+#             return False
+
+
+def check_url_validity(userinput):
+    response = requests.get(userinput)
+    if response.status_code < 400:
+        print("Valid.")
     else:
-        if re.search(compiled, userinput):
-            return True
-        else:
-            return False
+        print("Not valid.")
 
 
 ###--- DRIVER CODE ---###
 if __name__ == "__main__":
     userinput = input("Please enter a URL and hit Enter: ")
-    if check_url_validity_(userinput) == True:
-        print("Valid.")
-    else:
-        print("Not valid.")
+    check_url_validity(userinput)
+    # if check_url_validity(userinput) == True:
+    #     print("Valid.")
+    # else:
+    #     print("Not valid.")
